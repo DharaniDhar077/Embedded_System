8ccb6dc6ee62d056d4b23ee09813e6ac49303cf2
Dharani Dhar <dharanidhar077@gmail.com>
2022-04-24 11:53:47 +0530

    my first program

---
+++ arithmetic.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+	int a;
+	int b;
+	printf("Enter a value:");
+	scanf("%d: \n", &a);
+	printf("Enter b value:");
+	scanf("%d: \n", &b);
+	printf("%d", a+b);
+	printf("%d", a-b);
+	printf("%d", a*b);
+	return 0;
+}
