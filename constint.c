198239afa9a7f4a3b81a46e5c74a3d2b1f08b666
Dharani Dhar <dharanidhar077@gmail.com>
2022-04-24 20:21:07 +0530

    sevent program

---
+++ constint.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+	const int a = 10;
+	int b;
+	printf("Enter a value:);
+	scanf("%d: \n", &b);
+	printf("%d \n", a+b);
+	printf("%d \n", a-b);
+	printf("%d \n", a*b);
+	return 0;
+}
+	
