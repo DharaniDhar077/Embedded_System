3d8f96fc662bed83c91e1f3bf0368b2e0a64ea2f
Dharani Dhar <dharanidhar077@gmail.com>
2022-04-24 15:38:59 +0530

    my third program

---
+++ evenodd.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+	int a;
+	printf("Enter a value");
+	scanf("%d: \n", &a);
+	while (a<1)
+	if(a%2==0)
+	{
+		printf("Even Number");
+	}
+	else
+	{
+		printf("Odd Number");
+	}
+	return 0;
+}
+	
+		
